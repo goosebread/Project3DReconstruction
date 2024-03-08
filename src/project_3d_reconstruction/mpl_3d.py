6f1a78381a0d03cd599828dd59ef02295e920c5f
Brian Schubert <schubert.b@northeastern.edu>
2024-03-08 08:46:17 -0500

    Add WIP 3D->display coordinate transform notebook

--- src/project_3d_reconstruction/mpl_3d.py
+++ src/project_3d_reconstruction/mpl_3d.py
@@ -1,6 +1,8 @@
 """
 Tools for working 3D plots in `matplotlib`.
 """
+# Useful references:
+# - https://matplotlib.org/stable/users/explain/artists/transforms_tutorial.html
 
 from __future__ import annotations
 
@@ -15,12 +17,12 @@ if TYPE_CHECKING:
     from mpl_toolkits.mplot3d import Axes3D  # type: ignore[import-not-found]
 
 
-def axes_proj_image_coord(
+def axes_proj_display_coords(
     axes: Axes3D, coordinates: npt.ArrayLike
 ) -> npt.NDArray[np.float64]:
     """
-    Convert the given world 3D coordinates to their corresponding 2D image coordinates
-    in the given Axes' view box.
+    Convert the given world 3D coordinates to their corresponding 2D display
+    coordinates.
     """
     coordinates = np.asarray(coordinates)
     if coordinates.ndim != 2 or coordinates.shape[-1] != 3:  # noqa: PLR2004
@@ -29,6 +31,7 @@ def axes_proj_image_coord(
         )
 
     proj = axes.get_proj()
-    _transformed = homography.projective_transform(coordinates, proj)
+    ax_data_coords = homography.projective_transform(coordinates, proj)
+    display_coords = axes.transData.transform(ax_data_coords[..., :-1])
 
-    raise NotImplementedError
+    return display_coords  # type: ignore # noqa: RET504
