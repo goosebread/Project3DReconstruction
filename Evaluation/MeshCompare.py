6b4929347bfa86b12250de6b0f361e083ea4ea44
Nathan McGuire <nathan.e.mcguire@gmail.com>
2024-03-19 15:44:31 -0400

    Cleaned up directory structure a bit,

--- Evaluation/MeshCompare.py
+++
@@ -1,2 +0,0 @@
-# Need a way to compute the intersection over union ratio for mesh objects
-# May need optimization techniques/template matching to estimate the best alignment (or scaling if applicable)
